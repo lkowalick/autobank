e0ec085a4afe5626d56afb8ee668d1c410482302
Ryan Kowalick <rkowalick@lendinghome.com>
2015-12-15 23:24:49 -0500

    Successful functional spec

--- lib/autobank/parser.rb
+++ lib/autobank/parser.rb
@@ -1,23 +1,12 @@
+require 'csv'
+
 module Autobank
   module Parser
-    def self.parse(page)
-      raw_transactions = page.css("td").map(&:text)
-
-      raw_transactions.delete_at(-1)
-
-      transactions = []
-      raw_transactions.each_slice(8) do |s|
-        # The relevant data is in <td> tags, where the first is garbage, and
-        # only the 2nd, 4th, and 6th entries contain relevant data
-        new_trans = Autobank::Transaction.new(s[3], s[1], s[5])
-        transactions << new_trans
+    def self.parse(csv)
+      CSV.parse(csv, headers: true, header_converters: :symbol).map do |row|
+        amount = row[:amount].gsub(/\A(-?)(\d)/, '\1$\2')
+        Autobank::Transaction.new(row[:post_date], row[:description], amount)
       end
-
-      transactions.delete_if do |trans|
-        trans.date == "Pending"
-      end
-
-      transactions
     end
   end
 end
