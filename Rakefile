9d77ea7140fd7168f05ee8afdc7e49caf0923b55
Ryan Kowalick <rkowalick@lendinghome.com>
2016-05-28 09:27:58 -0400

    Update for new Chase CSV format

--- Rakefile
+++ Rakefile
@@ -1,1 +1,9 @@
 require "bundler/gem_tasks"
+require "rake/testtask"
+
+Rake::TestTask.new do |t|
+  t.test_files = FileList['spec/*spec.rb']
+  t.verbose = true
+end
+
+task default: :test
--- lib/autobank/parser.rb
+++ lib/autobank/parser.rb
@@ -5,7 +5,7 @@ module Autobank
     def self.parse(csv)
       CSV.parse(csv, headers: true, header_converters: :symbol).map do |row|
         amount = row[:amount].gsub(/\A(-?)(\d)/, '\1$\2')
-        Autobank::Transaction.new(date: row[:post_date],
+        Autobank::Transaction.new(date: row[:posting_date],
                                   payee: row[:description],
                                   amount: amount)
       end.sort_by(&:to_s)
